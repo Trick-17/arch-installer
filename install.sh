9b0c3d56cc852d61e3d03efad26f7b23d106cfe0
The N <n.n@n.n>
2016-07-21 13:51:37 +0200

    Fixed the broken select keyboard layout script.

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-bash /arch-installer/scripts/setup-keyboard.sh
+bash arch-installer/scripts/setup-keyboard.sh
--- scripts/setup-keyboard.sh
+++ scripts/setup-keyboard.sh
@@ -1,8 +1,25 @@
+#!/bin/bash
 echo "Please choose your keyboard-layout:"
-select yn in "DE - Deutschland" "DE - Schweiz" "EN - US"; do
+select yn in "DE - Deutschland" "DE - Schweiz" "EN - US";
+do 
     case $yn in
-        1 ) loadkeys de-latin1 ; echo KEYMAP=de-latin1 > arch-installer/vconsole.conf ; echo LANG=de_DE.UTF-8 > arch-installer/locale.conf; break;;
-        2 ) loadkeys sg-latin1; echo KEYMAP=sg-latin1 > arch-installer/vconsole.conf ; echo LANG=de_CH.UTF-8 > arch-installer/locale.conf ; break;;
-        3 ) echo LANG=en_US.UTF-8 > arch-installer/locale.conf ; break;;
+        "DE - Deutschland") 
+		loadkeys de-latin1 
+		echo KEYMAP=de-latin1 > arch-installer/vconsole.conf
+		echo LANG=de_DE.UTF-8 > arch-installer/locale.conf
+		break;;
+
+        "DE - Schweiz") 
+		loadkeys sg-latin1
+		echo KEYMAP=sg-latin1 > arch-installer/vconsole.conf
+		echo LANG=de_CH.UTF-8 > arch-installer/locale.conf
+		break;;
+
+        "EN - US") 
+		echo LANG=en_US.UTF-8 > arch-installer/locale.conf
+		break;;
+	* ) echo "Wrong!!!"
+            exit 1
     esac
+    break
 done
