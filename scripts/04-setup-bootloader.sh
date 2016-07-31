9f590a0425747c8ca2d5e977c011af82e239081f
Gideon <g.mueller@fz-juelich.de>
2016-07-31 14:53:35 +0200

    Fixed another bootloader bug

--- scripts/04-setup-bootloader.sh
+++ scripts/04-setup-bootloader.sh
@@ -4,7 +4,7 @@ cat <<-END > /mnt/boot/loader/entries/arch.conf
 title Arch Linux
 linux /vmlinuz-linux
 initrd /initramfs-linux.img
-options root=PARTUUID=$( blkid -s PARTUUID -o value $( blkid -L Arch ) )
+options root=PARTUUID=$( blkid -s PARTUUID -o value $( blkid -L Arch ) ) rw
 END
 
 cat <<-END > /mnt/boot/loader/loader.conf
