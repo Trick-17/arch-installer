1c08b3bd78aed801a44bd4dc4704af4672fe581b
Gideon <g.mueller@fz-juelich.de>
2016-08-20 22:42:02 +0200

    Default Theme in setup-desktop.

--- scripts/10-setup-desktop.sh
+++ scripts/10-setup-desktop.sh
@@ -5,3 +5,10 @@ sed -i 's/CursorTheme=/CursorTheme=breeze-dark/g' /mnt/etc/sddm.conf
 sed -i 's/Numlock=none/Numlock=on/g' /mnt/etc/sddm.conf
 
 arch-chroot /mnt systemctl enable sddm
+
+### Copy GTK theme file s.t. all users automatically have this
+cp arch-installer/.gtkrc-2.0 /mnt/etc/skel/
+
+### Copy KDE theme file s.t. all users automatically have this
+mkdir -p /mnt/etc/skel/.kde4/share/config/
+cp arch-installer/kdeglobals /mnt/etc/skel/.kde4/share/config/kdeglobals_zwei
