a59d1bc1c7be79790a9bfa1796862ce5ad7f2125
Gideon <g.mueller@fz-juelich.de>
2016-08-25 22:35:42 +0200

    Trying python for AUR packages.

--- scripts/12-setup-aur.sh
+++ scripts/12-setup-aur.sh
@@ -4,10 +4,13 @@
 # arch-chroot /mnt ./yaourt.sh
 
 ### Install aura
-chmod +x arch-installer/aura.sh
-cp arch-installer/aura.sh /mnt/
-arch-chroot /mnt ./aura.sh
+# chmod +x arch-installer/aura.sh
+# cp arch-installer/aura.sh /mnt/
+# arch-chroot /mnt ./aura.sh
 
 ### Install packages from AUR
 # arch-chroot /mnt yaourt -S visual-studio-code --noconfirm
-arch-chroot /mnt aura -S visual-studio-code
+# arch-chroot /mnt aura -S visual-studio-code
+
+cp arch-installer/aur-packages/install.py /mnt/
+arch-chroot /mnt python install.py
