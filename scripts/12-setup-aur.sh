7f61fe5a7f219e98b577a127844a668177b638b9
Gideon <g.mueller@fz-juelich.de>
2016-08-25 08:48:55 +0200

    Switched from yaourt to aura.

--- scripts/12-setup-aur.sh
+++ scripts/12-setup-aur.sh
@@ -1,7 +1,13 @@
 ### Install yaourt
-chmod +x arch-installer/yaourt.sh
-cp arch-installer/yaourt.sh /mnt/
-arch-chroot /mnt ./yaourt.sh
+# chmod +x arch-installer/yaourt.sh
+# cp arch-installer/yaourt.sh /mnt/
+# arch-chroot /mnt ./yaourt.sh
+
+### Install aura
+chmod +x arch-installer/aura.sh
+cp arch-installer/aura.sh /mnt/
+arch-chroot /mnt ./aura.sh
 
 ### Install packages from AUR
-arch-chroot /mnt yaourt -S visual-studio-code --noconfirm
+# arch-chroot /mnt yaourt -S visual-studio-code --noconfirm
+arch-chroot /mnt aura -S visual-studio-code
