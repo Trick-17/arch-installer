1178702c3aa84e5e7dddd2527831608d7279bef3
Gideon <g.mueller@fz-juelich.de>
2016-08-26 22:54:38 +0200

    Added output logs to script calls in install.sh.

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
-bash arch-installer/scripts/00-user-input.sh
-bash arch-installer/scripts/01-setup-partitions.sh
-bash arch-installer/scripts/02-setup-packages.sh
-bash arch-installer/scripts/03-setup-languages.sh
-bash arch-installer/scripts/04-setup-bootloader.sh
-bash arch-installer/scripts/05-setup-fstab.sh
-bash arch-installer/scripts/06-setup-timezone.sh
-bash arch-installer/scripts/07-setup-hostname.sh
-bash arch-installer/scripts/08-setup-network.sh
-bash arch-installer/scripts/09-setup-sshd.sh
-bash arch-installer/scripts/10-setup-desktop.sh
-bash arch-installer/scripts/11-setup-shell.sh
-bash arch-installer/scripts/12-setup-aur.sh
-bash arch-installer/scripts/13-setup-users.sh
-# bash arch-installer/scripts/14-setup-rootfs.sh
-# bash arch-installer/scripts/15-setup-personal.sh
+bash arch-installer/scripts/00-user-input.sh        > /mnt/etc/log_user_input.txt
+bash arch-installer/scripts/01-setup-partitions.sh  > /mnt/etc/log_setup_partitions.txt
+bash arch-installer/scripts/02-setup-packages.sh    > /mnt/etc/log_setup_packages.txt
+bash arch-installer/scripts/03-setup-languages.sh   > /mnt/etc/log_setup_languages.txt
+bash arch-installer/scripts/04-setup-bootloader.sh  > /mnt/etc/log_setup_bootloader.txt
+bash arch-installer/scripts/05-setup-fstab.sh       > /mnt/etc/log_setup_fstab.txt
+bash arch-installer/scripts/06-setup-timezone.sh    > /mnt/etc/log_setup_timezone.txt
+bash arch-installer/scripts/07-setup-hostname.sh    > /mnt/etc/log_setup_hostname.txt
+bash arch-installer/scripts/08-setup-network.sh     > /mnt/etc/log_setup_network.txt
+bash arch-installer/scripts/09-setup-sshd.sh        > /mnt/etc/log_setup_sshd.txt
+bash arch-installer/scripts/10-setup-desktop.sh     > /mnt/etc/log_setup_desktop.txt
+bash arch-installer/scripts/11-setup-shell.sh       > /mnt/etc/log_setup_shell.txt
+bash arch-installer/scripts/12-setup-aur.sh         > /mnt/etc/log_setup_aur.txt
+bash arch-installer/scripts/13-setup-users.sh       > /mnt/etc/log_setup_users.txt
+# bash arch-installer/scripts/14-setup-personal.sh    > /mnt/etc/log_setup_personal.txt
+# bash arch-installer/scripts/15-setup-rootfs.sh      > /mnt/etc/log_setup_rootfs.txt
