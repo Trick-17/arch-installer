71c15ee1c8bd809b961b8d6ab3f8d5b6eaedbd0d
NOhs <NOhs@users.noreply.github.com>
2016-08-30 23:50:23 +0200

    Clarified descriptive output.

--- scripts/01-setup-partitions.sh
+++ scripts/01-setup-partitions.sh
@@ -1,12 +1,12 @@
-echo " >> Fetching devices from labels 'Arch' and 'EFI'"
+echo " >> Fetching partitions from labels 'Arch' and 'EFI'"
 device=$( blkid -L Arch )
 bootdevice=$( blkid -L EFI )
 
-echo " >> Mounting Arch partition on /mnt"
+echo " >> Mounting 'Arch' partition on /mnt"
 yes | mkfs.ext4 -L Arch $device
 mount $device /mnt
 
-echo " >> Mounting boot device on EFI partition"
+echo " >> Mounting 'EFI' boot partition on /mnt/boot"
 mkdir -p /mnt/boot
 yes | mkfs.fat -F32 $bootdevice 
 fatlabel $bootdevice EFI
