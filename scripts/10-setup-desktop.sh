a172268a2decdcd3a671fc3c14faf3f4a555d65d
NOhs <NOhs@users.noreply.github.com>
2016-09-11 01:55:18 +0200

    Fixed bug in setup desktop.
    Changed the `sed` call to be more flexible in finding the current theme and replacing it with breeze for the login screen.

--- scripts/10-setup-desktop.sh
+++ scripts/10-setup-desktop.sh
@@ -9,7 +9,7 @@ then
 
     arch-chroot /mnt sddm --example-config > /mnt/etc/sddm.conf
 
-    sed -i 's/Current=maui/Current=breeze/g' /mnt/etc/sddm.conf
+    sed -i 's/^Current=.*/Current=breeze/g' /mnt/etc/sddm.conf
     sed -i 's/CursorTheme=/CursorTheme=breeze-dark/g' /mnt/etc/sddm.conf
     sed -i 's/Numlock=none/Numlock=on/g' /mnt/etc/sddm.conf
 
