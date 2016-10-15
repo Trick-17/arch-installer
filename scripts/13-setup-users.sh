bf87f902a9f075fdb9e06c8f9f10b2cf52f3b487
NOhs <nicholas.ohs@outlook.com>
2016-10-15 20:37:32 +0200

    Added setting root password during install.

--- scripts/13-setup-users.sh
+++ scripts/13-setup-users.sh
@@ -4,8 +4,11 @@
 source arch-installer/user-input.txt
 
 arch-chroot /mnt useradd -m -G wheel -s /bin/bash $USER_USERNAME
-echo "Please type in a passwort for ${USER_USERNAME}:"
+echo "Please type in a password for ${USER_USERNAME}:"
 arch-chroot /mnt passwd $USER_USERNAME
+
+echo "Please type in a password for `root` user:"
+arch-chroot /mnt passwd
 
 ### Place the user into the sudoers list
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /mnt/etc/sudoers
