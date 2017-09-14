16d6acff8b45584bbddaf0d5160017ebae73f6ab
Gideon <g.mueller@fz-juelich.de>
2017-09-14 18:53:01 +0200

    Added NetworkManager to autostart.

--- pyscripts/s10_network.py
+++ pyscripts/s10_network.py
@@ -5,5 +5,6 @@ def configure_network():
     print(" >> Enabling dhcpd")
     try:
         run('arch-chroot /mnt systemctl enable dhcpcd')
+        run('arch-chroot /mnt systemctl enable NetworkManager')
     except CalledProcessError as error:
         print('Enabling dhcpd failed with message: ', error.output)
